0c1d2ad6d70bc830e8fe288e6a7a599f91359a48
Ghaith <gboksm11@uw.edu>
2023-02-14 11:15:37 -0800

    added feedback changes 2 - missing updated logo

--- src/components/HomeCarousel/HomeCarousel.jsx
+++ src/components/HomeCarousel/HomeCarousel.jsx
@@ -3,6 +3,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a lo
 import img_1 from "../../assets/carousel-img-1.jpg";
 import img_2 from "../../assets/carousel-img-2.png";
 import img_3 from "../../assets/carousel-img-3.jpg";
+import img_4 from "../../assets/carousel-img-4.png";
 
 
 const HomeCarousel = () => {
@@ -13,7 +14,7 @@ const HomeCarousel = () => {
             infiniteLoop={true}
             centerMode={true}
             centerSlidePercentage={100}
-            dynamicHeight={false}
+            dynamicHeight={true}
             showThumbs={false}
             showStatus={false}
             showIndicators={false}
@@ -26,7 +27,7 @@ const HomeCarousel = () => {
             </div>
 
             <div>
-                <img src={img_3} />
+                <img src={img_4} />
             </div>
         </Carousel>
     )
